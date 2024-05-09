3863d7549644464accb63b7fb044754bdd4a9836
Abubakr <abubakrdev69@gmail.com>
2024-05-09 20:42:04 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import Products from './components/product/Products'
+
+
+function App() {
+  return (
+    <div>
+      <Products />
+    </div>
+  )
+}
+
+export default App
